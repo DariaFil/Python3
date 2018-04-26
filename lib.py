572e473baba39d6b1648f016461c90ceaf065cc4
DariaFil <36414448+DariaFil@users.noreply.github.com>
2018-04-27 00:38:56 +0300

    Fixed documentation

--- lib.py
+++ lib.py
@@ -1,124 +1,163 @@
-'''Общий абстрактный класс объектов поля'''
-class Playing_object:
-    '''Код объекта, позволяющий переопределить равенство объектов и их хэширование.
-        Код для каждого объекта уникален'''
+class PlayingObject:
+    """
+Общий абстрактный класс объектов поля
+object_code - код объекта, позволяющий переопределить равенство объектов и их хэширование.
+Код для каждого объекта уникален
+    """
+
     object_code = 0
 
     def __init__(self):
         pass
 
     def __eq__(self, other):
-        if self.object_code == other.object_code:
-            return True
-        else:
-            return False
+        return self.object_code == other.object_code
 
     def __hash__(self):
         return self.object_code
 
-    '''Функция поведения объекта при изменении состояния поля, переопределённая в каждом из потомков'''
     def update_cell(self, life, i, j):
+        """
+        Функция поведения объекта при изменении состояния поля, переопределённая в каждом из потомков
+        :param life: текущая игра, в которой содержится объект
+        :param i: х-координата объекта
+        :param j: у-координата объекта
+        :return: новый объект клетки типа, наследуемого от PlayingObject
+        """
         pass
 
 
-'''Класс ячейки со скалами'''
-class Rock(Playing_object):
+class Rock(PlayingObject):
+    """
+Класс ячейки со скалами
+    """
     object_code = 1
 
     def update_cell(self, life, i, j):
-        rock = Rock() #Возвращаемый объект
-        return rock
+        return Rock()  # Возвращаемый объект
 
 
-'''Класс ячейки с рыбой'''
-class Fish(Playing_object):
+class Fish(PlayingObject):
+    """
+Класс ячейки с рыбой
+    """
     object_code = 2
 
     def update_cell(self, life, i, j):
         if life.count_neighbours(i, j, 'f') > 3 or life.count_neighbours(i, j, 'f') < 2:
-            ocean = Ocean() #Возвращаемый объект
-            return ocean
+            return Ocean()  # Возвращаемый объект
         else:
-            fish = Fish() #Возвращаемый объект
-            return fish
+            return Fish()  # Возвращаемый объект
 
 
-'''Класс ячейки пустого океана'''
-class Ocean(Playing_object):
+class Ocean(PlayingObject):
+    """
+Класс ячейки пустого океана
+    """
     object_code = 0
 
     def update_cell(self, life, i, j):
         if life.count_neighbours(i, j, 'f') == 3:
-            fish = Fish() #Возвращаемый объект
-            return fish
+            return Fish()  # Возвращаемый объект
         elif life.count_neighbours(i, j, 's') == 3:
-            shrimp = Shrimp() #Возвращаемый объект
-            return shrimp
+            return Shrimp()  # Возвращаемый объект
         else:
-            ocean = Ocean() #Возвращаемый объект
-            return ocean
+            return Ocean()  # Возвращаемый объект
 
 
-'''Класс ячейки с креветкой'''
-class Shrimp(Playing_object):
+class Shrimp(PlayingObject):
+    """
+Класс ячейки с креветкой
+    """
     object_code = 3
 
     def update_cell(self, life, i, j):
         if life.count_neighbours(i, j, 's') > 3 or life.count_neighbours(i, j, 's') < 2:
-            ocean = Ocean() #Возвращаемый объект
-            return ocean
+            return Ocean()  # Возвращаемый объект
         else:
-            shrimp = Shrimp() #Возвращаемый объект
-            return shrimp
+            return Shrimp()  # Возвращаемый объект
 
 
-class CLifegame(object):
+class LifeGame(object):
+    """
+Класс игры
+Метод задаётся высотой поля и длиной поля
+    """
+
     def __init__(self, _height, _length):
-        self.height = _height # Высота игрового поля
-        self.length = _length #Длна строки игрового поля
-        self.game_field = [[None]*self.length for i in range(self.height)] #Игровое поле
-        self.def_ocean = Ocean() #Экземпляр объекта класса для составления словарей
-        self.def_rock = Rock() #Экземпляр объекта класса для составления словарей
-        self.def_fish = Fish() #Экземпляр объекта класса для составления словарей
-        self.def_shrimp = Shrimp() #Экземпляр объекта класса для составления словарей
-        '''Словари для преобразования символа поля в класс объекта, заданного этим символом, и обратно'''
-        self.dictSymbol_to_object = {"n": self.def_ocean, "r": self.def_rock, "f": self.def_fish, "s": self.def_shrimp}
-        self.dictObject_to_symbol = {self.def_ocean: "n", self.def_rock: "r", self.def_fish: "f", self.def_shrimp: "s"}
+        self.height = _height  # Высота игрового поля
+        self.length = _length  # Длна строки игрового поля
+        self.game_field = [[None]*self.length for i in range(self.height)]  # Игровое поле
+        self.def_ocean = Ocean()  # Экземпляр объекта класса для составления словарей
+        self.def_rock = Rock()  # Экземпляр объекта класса для составления словарей
+        self.def_fish = Fish()  # Экземпляр объекта класса для составления словарей
+        self.def_shrimp = Shrimp()  # Экземпляр объекта класса для составления словарей
+        """
+Словари для преобразования символа поля в класс объекта, заданного этим символом, и обратно
+        """
+        self.dictSymbol_to_object = {'n': self.def_ocean, 'r': self.def_rock, 'f': self.def_fish, 's': self.def_shrimp}
+        self.dictObject_to_symbol = {self.def_ocean: 'n', self.def_rock: 'r', self.def_fish: 'f', self.def_shrimp: 's'}
 
-    '''Заполнение масиива поля игры'''
-    def full_field(self, x, string_of_field): #Номер строки, содержание строки
+    def full_field(self, x, string_of_field):
+        """
+        Заполнение массива поля игры
+        :param x: номер строки
+        :param string_of_field: содежрание строки
+        """
         self.game_field[x] = string_of_field
 
-    def get_cell(self, x, y): #
+    def get_cell(self, x, y):
+        """
+        Возвращает значение игровой клетки
+        :param x: х-координата клетки
+        :param y: у-координата клетки
+        :return: символ - игровой объект в ячейке
+        """
         return self.game_field[x][y]
 
-    '''Проверка существования ячейки с данными координатами'''
-    def is_field_cell_exist(self, x, y): #Координаты ячейки
-        if 0 <= x < self.height and 0 <= y < self.length:
-            return True
-        else:
-            return False
+    def _is_field_cell_exist(self, x, y):
+        """
+        Проверка существования ячейки с данными координатами
+        :param x: х-координата клетки
+        :param y: у-координата клетки
+        :return: True, если клетка игрового поля существует,
+                 False, если клетка находится за границами игрового поля
+        """
+        return 0 <= x < self.height and 0 <= y < self.length
 
-    '''Подсчёт соседей того же типа, что и объект в данной ячейке'''
-    def count_neighbours(self, x, y, obj): #Координаты ячейка и объект, находящийся в ней
-        number_of_neighbours = 0 #Количество соседей одного вида с объектом в клетке
+    def count_neighbours(self, x, y, obj):
+        """
+        Подсчёт соседей того же типа, что и объект в данной ячейке
+        :param x: х-координата клетки
+        :param y: у-координата клетки
+        :param obj: игровой объект, находящийся в данной ячейке
+        :return: количество соседей данного типа объектов
+        """
+        number_of_neighbours = 0  # Количество соседей одного вида с объектом в клетке
         for x_inc in range(-1, 2):
             for y_inc in range(-1, 2):
                 if not(x_inc == 0 and y_inc == 0):
-                    if self.is_field_cell_exist(x + x_inc, y + y_inc) and self.game_field[x + x_inc][y + y_inc] == obj:
+                    if self._is_field_cell_exist(x + x_inc, y + y_inc) and self.game_field[x + x_inc][y + y_inc] == obj:
                         number_of_neighbours += 1
         return number_of_neighbours
 
-    '''Переход к следующему состоянию океана'''
     def next_game_state(self):
-        next_state_game_field = [[None]*self.length for l in range(self.height)] #Результат новой итерации игры
+        """
+        Переход к следующему состоянию океана
+        В результате обновляется состояние игрового поля
+        """
+        next_state_game_field = [[None]*self.length for l in range(self.height)]  # Результат новой итерации игры
         for i in range(self.height):
             for j in range(self.length):
-                obj = self.dictSymbol_to_object[self.game_field[i][j]] #Объект, находящийся в клетке, расшифрованный по словарю
-                next_state_game_field[i][j] = self.dictObject_to_symbol[obj.update(self, i, j)]
+                obj = self.dictSymbol_to_object[self.game_field[i][j]]  # Объект, находящийся в клетке, расшифрованный по словарю
+                next_state_game_field[i][j] = self.dictObject_to_symbol[obj.update_cell(self, i, j)]
         self.game_field = next_state_game_field
 
-    '''Запуск игры '''
     def play(self, steps):
+        """
+        Запуск игры на определённое количество итераций
+        :param steps: количество игровых итераций
+        :return: обновляется состояние игры
+        """
         for i in range(steps):
             self.next_game_state()
--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import sys
 
 def set_parser_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--inputfile', action='store') #Имя входного файла
-    parser.add_argument('--outputfile', action='store') #Имя выходного поля
-    parser.add_argument('--tests', action='store_true') #Флаг для запуска тестов
+    parser.add_argument('--inputfile', action='store')  # Имя входного файла
+    parser.add_argument('--outputfile', action='store')  # Имя выходного поля
+    parser.add_argument('--tests', action='store_true')  # Флаг для запуска тестов
     return parser.parse_args()
 
 
@@ -19,10 +19,10 @@ def run():
     else:
         if args.inputfile is not None:
             sys.stdin = open(args.inputfile, "r")
-        height, length, steps = list(map(int, input().split())) #Высота, длина игрового поля и количество итераций игры
-        game = lib.CLifegame(height, length) #Непосрадственоо игра
+        height, length, steps = list(map(int, input().split()))  # Высота, длина игрового поля и количество итераций игры
+        game = lib.LifeGame(height, length)  # Инициация игры
         for i in range(height):
-            string_of_field = list(input()) #Строка игрового поля
+            string_of_field = list(input())  # Строка игрового поля
             game.full_field(i, string_of_field)
         game.play(steps)
         if args.outputfile is not None:
