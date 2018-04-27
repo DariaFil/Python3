ca211e00b6da8cc90997b5777cdafd8842d6611e
DariaFil <36414448+DariaFil@users.noreply.github.com>
2018-04-28 01:23:54 +0300

    Изменена структура поведения объектов игры
    Для добавления объектов на поле создана фабрика; удалены все словари; добавлено поле символа объекта вместо поля кода; игра полностью не зависит от количества типов игровых объектов и их поведения при переходе к следующей итерации. Игра содержит метод return_neighbours для передачи списка соседей объектам при их обновлении; если будет добавлен тип объекта, не зависящий от списка соседей или зависящий по-иному, то для него всё равно надо будет отдельно прописать функцию его поведения, поэтому метод return_neighbours можно и целесообразно оставить.

--- lib.py
+++ lib.py
@@ -5,98 +5,124 @@ object_code - код объекта, позволяющий переопреде
 Код для каждого объекта уникален
     """
 
-    object_code = 0
+    symbol = ''
 
     def __init__(self):
         pass
 
     def __eq__(self, other):
-        return self.object_code == other.object_code
+        return self.symbol == other.symbol
 
-    def __hash__(self):
-        return self.object_code
-
-    def update_cell(self, life, i, j):
+    def update_cell(self, game, x, y):
         """
         Функция поведения объекта при изменении состояния поля, переопределённая в каждом из потомков
-        :param life: текущая игра, в которой содержится объект
-        :param i: х-координата объекта
-        :param j: у-координата объекта
-        :return: новый объект клетки типа, наследуемого от PlayingObject
+        :param game: Игра, из которой взят объект
+        :param x: х-координата объекта на поле игры
+        :param y: у-координата объекта на поле игры
+        :return: объект, получившийся после обновления ячейки
         """
         pass
-
-
-class Rock(PlayingObject):
-    """
-Класс ячейки со скалами
-    """
-    object_code = 1
-
-    def update_cell(self, life, i, j):
-        return Rock()  # Возвращаемый объект
-
-
-class Fish(PlayingObject):
-    """
-Класс ячейки с рыбой
-    """
-    object_code = 2
-
-    def update_cell(self, life, i, j):
-        if life.count_neighbours(i, j, 'f') > 3 or life.count_neighbours(i, j, 'f') < 2:
-            return Ocean()  # Возвращаемый объект
-        else:
-            return Fish()  # Возвращаемый объект
 
 
 class Ocean(PlayingObject):
     """
 Класс ячейки пустого океана
     """
-    object_code = 0
+    symbol = "n"
 
-    def update_cell(self, life, i, j):
-        if life.count_neighbours(i, j, 'f') == 3:
-            return Fish()  # Возвращаемый объект
-        elif life.count_neighbours(i, j, 's') == 3:
-            return Shrimp()  # Возвращаемый объект
+    def update_cell(self, game, x, y):
+        neighbours = game.return_neighbours(x, y)  # Массив объектов-соседей
+        fish_count = 0  # Количество рыб среди соседей
+        shrimp_count = 0  # Количество креветок среди соседей
+        for i in neighbours:
+            if i == Fish():
+                fish_count += 1
+            elif i == Shrimp:
+                shrimp_count += 1
+        if fish_count == 3:
+            return Fish()
+        elif shrimp_count == 3:
+            return Shrimp()
         else:
-            return Ocean()  # Возвращаемый объект
+            return Ocean()
+
+
+class Rock(PlayingObject):
+    """
+Класс ячейки со скалами
+    """
+    symbol = "r"
+
+    def update_cell(self, game, x, y):
+        return Rock()
+
+
+class Fish(PlayingObject):
+    """
+Класс ячейки с рыбой
+    """
+    symbol = "f"
+
+    def update_cell(self, game, x, y):
+        neighbours = game.return_neighbours(x, y)  # Массив объектов-соседей
+        fish_count = 0  # Количество рыб среди соседей
+        for i in neighbours:
+            if i == Fish():
+                fish_count += 1
+        if 1 < fish_count < 4:
+            return Fish()
+        else:
+            return Ocean()
 
 
 class Shrimp(PlayingObject):
     """
 Класс ячейки с креветкой
     """
-    object_code = 3
+    symbol = "s"
 
-    def update_cell(self, life, i, j):
-        if life.count_neighbours(i, j, 's') > 3 or life.count_neighbours(i, j, 's') < 2:
-            return Ocean()  # Возвращаемый объект
+    def update_cell(self, game, x, y):
+        neighbours = game.return_neighbours(x, y)  # Массив объектов-соседей
+        shrimp_count = 0  # Количество креветок среди соседей
+        for i in neighbours:
+            if i == Fish():
+                shrimp_count += 1
+        if 1 < shrimp_count < 4:
+            return Shrimp()
         else:
-            return Shrimp()  # Возвращаемый объект
+            return Ocean()
 
 
-class LifeGame(object):
+class Factory:
+    """
+    Фабрика игровых объектов
+    Метод __init__ инициирует массив игровых объектов - потомков PlayingObject
+    """
+    def __init__(self):
+        self.object_list = PlayingObject.__subclasses__()  # Массив слабых ссылок на все типы игровых объектов
+
+    def get_object(self, symbol):
+        """
+        Возвращает объект по его символу
+        :param symbol: символ объекта
+        :return: игровой объект
+        """
+        i = 0
+        while self.object_list[i].symbol != symbol:
+            i += 1
+        return self.object_list[i]()
+
+
+class LifeGame:
     """
 Класс игры
-Метод задаётся высотой поля и длиной поля
+Метод __init__ задаётся высотой поля и длиной поля и инициирует, помимо
+параметров, поле игры в незаполненном варианте
     """
-
     def __init__(self, _height, _length):
         self.height = _height  # Высота игрового поля
         self.length = _length  # Длна строки игрового поля
-        self.game_field = [[None]*self.length for i in range(self.height)]  # Игровое поле
-        self.def_ocean = Ocean()  # Экземпляр объекта класса для составления словарей
-        self.def_rock = Rock()  # Экземпляр объекта класса для составления словарей
-        self.def_fish = Fish()  # Экземпляр объекта класса для составления словарей
-        self.def_shrimp = Shrimp()  # Экземпляр объекта класса для составления словарей
-        """
-Словари для преобразования символа поля в класс объекта, заданного этим символом, и обратно
-        """
-        self.dictSymbol_to_object = {'n': self.def_ocean, 'r': self.def_rock, 'f': self.def_fish, 's': self.def_shrimp}
-        self.dictObject_to_symbol = {self.def_ocean: 'n', self.def_rock: 'r', self.def_fish: 'f', self.def_shrimp: 's'}
+        self.game_field = [[PlayingObject]*self.length for i in range(self.height)]  # Игровое поле
 
     def full_field(self, x, string_of_field):
         """
@@ -104,7 +130,9 @@ class LifeGame(object):
         :param x: номер строки
         :param string_of_field: содежрание строки
         """
-        self.game_field[x] = string_of_field
+        factory = Factory()
+        for i in range(len(string_of_field)):
+            self.game_field[x][i] = factory.get_object(string_of_field[i])
 
     def get_cell(self, x, y):
         """
@@ -113,9 +141,9 @@ class LifeGame(object):
         :param y: у-координата клетки
         :return: символ - игровой объект в ячейке
         """
-        return self.game_field[x][y]
+        return self.game_field[x][y].symbol
 
-    def _is_field_cell_exist(self, x, y):
+    def __is_field_cell_exist(self, x, y):
         """
         Проверка существования ячейки с данными координатами
         :param x: х-координата клетки
@@ -125,32 +153,30 @@ class LifeGame(object):
         """
         return 0 <= x < self.height and 0 <= y < self.length
 
-    def count_neighbours(self, x, y, obj):
+    def return_neighbours(self, x, y):
         """
         Подсчёт соседей того же типа, что и объект в данной ячейке
         :param x: х-координата клетки
         :param y: у-координата клетки
-        :param obj: игровой объект, находящийся в данной ячейке
         :return: количество соседей данного типа объектов
         """
-        number_of_neighbours = 0  # Количество соседей одного вида с объектом в клетке
+        neighbours_counts = []  # Массив соседей-объектов
         for x_inc in range(-1, 2):
             for y_inc in range(-1, 2):
                 if not(x_inc == 0 and y_inc == 0):
-                    if self._is_field_cell_exist(x + x_inc, y + y_inc) and self.game_field[x + x_inc][y + y_inc] == obj:
-                        number_of_neighbours += 1
-        return number_of_neighbours
+                    if self.__is_field_cell_exist(x + x_inc, y + y_inc):
+                        neighbours_counts.append(self.game_field[x + x_inc][y + y_inc])
+        return neighbours_counts
 
-    def next_game_state(self):
+    def __next_game_state(self):
         """
         Переход к следующему состоянию океана
         В результате обновляется состояние игрового поля
         """
-        next_state_game_field = [[None]*self.length for l in range(self.height)]  # Результат новой итерации игры
+        next_state_game_field = [[PlayingObject]*self.length for l in range(self.height)]  # Результат новой итерации игры
         for i in range(self.height):
             for j in range(self.length):
-                obj = self.dictSymbol_to_object[self.game_field[i][j]]  # Объект, находящийся в клетке, расшифрованный по словарю
-                next_state_game_field[i][j] = self.dictObject_to_symbol[obj.update_cell(self, i, j)]
+                next_state_game_field[i][j] = self.game_field[i][j].update_cell(self, i, j)  # Новый объект в текущей ячейке
         self.game_field = next_state_game_field
 
     def play(self, steps):
@@ -160,4 +186,4 @@ class LifeGame(object):
         :return: обновляется состояние игры
         """
         for i in range(steps):
-            self.next_game_state()
+            self.__next_game_state()
